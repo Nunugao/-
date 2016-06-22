395ec6a31d00b57084853f8f67289686f1d590a1
nunu <nunufight@163.com>
2016-06-22 15:01:19 +0800

    Initial Commit

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by NuNu on 16/6/22.
+//  Copyright © 2016年 NuNu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
